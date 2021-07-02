3b81e2bd6314af5f3887af0d8ac6b19dbcded929
Muhammad wasi <wasiarain819@gmail.com>
2021-07-02 23:33:13 +0500

    Update: Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,9 @@ import React from "react";
 import CreateTask from "./CreateTask";
 import TaskList from "./TaskList";
 
-const tasks = [];
-
+const tasks = localStorage.getItem("tasks")
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -18,20 +19,24 @@ export default class Main extends React.Component {
     }
     tasks.push({ task, isCompleted: false });
     this.setState({ tasks: tasks });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
   toggleTask = (taskId) => {
     const taskItem = tasks[taskId];
     taskItem.isCompleted = !taskItem.isCompleted;
     this.setState({ tasks: tasks });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
   deleteTask = (taskId) => {
     tasks.splice(taskId, 1);
     this.setState({ tasks: tasks });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
   editTask = (taskId, task) => {
     const taskItem = tasks[taskId];
     taskItem.task = task;
     this.setState({ tasks: tasks });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
   render() {
     return (
@@ -44,7 +49,8 @@ export default class Main extends React.Component {
             tasks={this.state.tasks}
             deleteTask={this.deleteTask}
             editTask={this.editTask}
-            toggleTask={this.toggleTask}/>
+            toggleTask={this.toggleTask}
+          />
         </div>
       </div>
     );
